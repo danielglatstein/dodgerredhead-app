4d37dd40503a558d2e4c0312843cd1622b8bd917
Daniel Glatstein <danielglatstein@gmail.com>
2015-05-11 14:48:06 -0400

    add in_groups_of fix, add rmagick recreate_versions! fix, add asset pipeline fix

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,6 +5,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
  include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
+  #include Sprockets::Helpers::RailsHelper
+  #include Sprockets::Helpers::IsolatedHelper
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -17,10 +19,17 @@ class ImageUploader < CarrierWave::Uploader::Base
    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
- version :thumb do
-    process :resize_to_limit => [200, 200]
+  version :thumb do
+    process :resize_to_fill => [900, 500]
   end
  
+
+
+ # def resize(thumb)
+  #  manipulate! format: "png" do |source|
+    #  source = source.resize_to_fill(451, 283)
+   # end 
+  #end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
@@ -41,9 +50,9 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+   def extension_white_list
+     %w(jpg jpeg gif png)
+   end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
